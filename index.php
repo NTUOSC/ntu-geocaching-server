d0e7539c9416e61ef4e5e9618b53988ef5d702f1
Shouko <h999342@gmail.com>
2014-08-09 00:57:19 +0800

    Add endpoint managing interface

--- index.php
+++ index.php
@@ -41,6 +41,55 @@ $app->get('/', function(){
 
 });
 
+$app->post('/endpoint', function(){
+
+	// MASTER_KEY_SHA1 should be a SHA1 string and set in ENV vars
+	// It is used to manage endpoints
+
+	if( sha1($_POST['auth']) === getenv("MASTER_KEY_SHA1") ){
+
+		$endpoint = R::findOne('endpoint', ' name = ? ', [ $_POST['name'] ]);
+
+		$action = '';
+
+		if(empty($endpoint)){
+
+			// Adding new endpoint
+
+			$action = 'add';
+			$endpoint = R::dispense('endpoint');
+			$endpoint['name'] = $_POST['name'];
+
+		}else{
+
+			// Updating existing endpoint
+
+			$action = 'update';
+			$endpoint = R::load('endpoint', $endpoint['id']);
+
+		}
+
+		$endpoint['key'] = $_POST['key'];
+
+		R::store( $endpoint );
+
+		echo json_encode(
+			array(
+				"result" => "ok",
+				"message" => "Endpoint ".$action."ed sucessfully!"
+			)
+		);
+
+	}else{
+
+		// Wrong key
+
+		header("HTTP/1.1 403 Forbidden");
+
+	}
+
+});
+
 $app->run();
 
 ?>
